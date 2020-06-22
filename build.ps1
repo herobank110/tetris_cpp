0104627101bd72ce00f56fd3efd502f2bb82b2b5
David <dkanekanian1@gmail.com>
2020-06-22 14:49:01 +0100

    Invert colors

--- build.ps1
+++ build.ps1
@@ -1,3 +1,3 @@
-em++ .\tetris.cpp -s WASM=1 -o index.html -s USE_SDL=2
+em++ .\tetris.cpp -s WASM=1 -o index.html -s USE_SDL=2 -std=c++17
 # --shell-file  ..\my_template.html 
 # -s NO_EXIT_RUNTIME=1 -s "EXTRA_EXPORTED_RUNTIME_METHODS=['ccall']"
--- tetris.cpp
+++ tetris.cpp
@@ -9,90 +9,112 @@
 SDL_Window *window;
 SDL_Renderer *renderer;
 std::unique_ptr<class Board> board;
+std::unique_ptr<class GameMode> game_mode;
 
-class Board
+class Actor
 {
-    constexpr static const int width = 10;
-    constexpr static const int height = 21;
-    std::vector<bool> data;
+public:
+  void tick(float delta_time);
+};
+
+class Board : Actor
+{
+  constexpr static const int width = 10;
+  constexpr static const int height = 21;
+  std::vector<bool> data;
 
 public:
-    Board()
+  Board()
+  {
+    data.reserve(width * height);
+    data.assign(width * height, false);
+
+    data[0] = true;
+    data[11] = true;
+    data[22] = true;
+    data[33] = true;
+  }
+
+  void tick(float delta_time) { print(); }
+
+  void print()
+  {
+    const int grid_size = 20, padding = 2;
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+    int w, h;
+    SDL_GetWindowSize(window, &w, &h);
+    const int right_edge = (w - grid_size * width) / 2;
+    SDL_Rect rect{right_edge, h - 2 * (grid_size - padding), grid_size - padding, grid_size - padding};
+
+    for (int i = 0; i < data.size(); i++)
     {
-        data.reserve(width * height);
-        data.assign(width * height, false);
-
-        data[0] = true;
-        data[11] = true;
-        data[22] = true;
-        data[33] = true;
-    }
-
-    void print()
-    {
-        const int grid_size = 20, padding = 2;
-
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
-        int w, h;
-        SDL_GetWindowSize(window, &w, &h);
-        const int right_edge = (w - grid_size * width) / 2;
-        SDL_Rect rect{right_edge, h - 2 * (grid_size - padding), grid_size - padding, grid_size - padding};
-
-        for (int i = 0; i < data.size(); i++)
+      if (i != 0)
+      {
+        rect.x += grid_size;
+        if (i % width == 0)
         {
-            if (i != 0)
-            {
-                rect.x += grid_size;
-                if (i % width == 0)
-                {
-                    // New row above.
-                    rect.y -= grid_size;
-                    rect.x = right_edge;
-                }
-            }
-
-            if (data[i])
-            {
-                SDL_RenderFillRect(renderer, &rect);
-            }
-            else
-            {
-                SDL_RenderDrawRect(renderer, &rect);
-            }
+          // New row above.
+          rect.y -= grid_size;
+          rect.x = right_edge;
         }
+      }
+
+      if (data[i])
+      {
+        SDL_RenderFillRect(renderer, &rect);
+      }
+      else
+      {
+        SDL_RenderDrawRect(renderer, &rect);
+      }
     }
+  }
+};
+
+class GameMode : Actor
+{
+public:
+  void tick(float delta_time)
+  {
+  }
 };
 
 void render_func(void)
 {
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
+  SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+  SDL_RenderClear(renderer);
 
-    board->print();
+  // Assume 60 fps.
+  const float delta_time = 1000.f / 60.f;
 
-    SDL_RenderPresent(renderer);
+  board->tick(delta_time);
+  game_mode->tick(delta_time);
+
+  SDL_RenderPresent(renderer);
 
 #ifdef EMSCRIPTEN
-    emscripten_cancel_main_loop();
+  emscripten_cancel_main_loop();
 #endif
 }
 
 int main()
 {
-    assert(SDL_Init(SDL_INIT_VIDEO) == 0);
-    SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
+  assert(SDL_Init(SDL_INIT_VIDEO) == 0);
+  SDL_CreateWindowAndRenderer(800, 600, 0, &window, &renderer);
 
-    board = std::make_unique<Board>();
+  board = std::make_unique<Board>();
+  game_mode = std::make_unique<GameMode>();
 
 #ifdef EMSCRIPTEN
-    emscripten_set_main_loop(render_func, 0, 1);
+  emscripten_set_main_loop(render_func, 0, 1);
 #else
-    render_func();
+  render_func();
 #endif
 
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }
